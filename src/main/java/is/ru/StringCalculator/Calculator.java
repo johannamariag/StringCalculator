e41c8b72a1d2e7681e33dac049a4cefdc9a5a7b4
Johanna Maria <johannamariagislad@gmail.com>
2017-10-16 19:29:25 +0000

    Testing unknown amount of numbers

--- src/main/java/is/ru/StringCalculator/Calculator.java
+++ src/main/java/is/ru/StringCalculator/Calculator.java
@@ -28,4 +28,5 @@ public class Calculator {
     	return total;
     }
 
+
 }
--- src/test/java/is/ru/StringCalculator/CalculatorTest.java
+++ src/test/java/is/ru/StringCalculator/CalculatorTest.java
@@ -20,8 +20,14 @@ public class CalculatorTest{
 	public void testMultipleNumbers() {
 		assertEquals(6, Calculator.add("1,2,3"));
 	}
-
-
+	@Test
+    public void testLineSplit(){
+    	assertEquals(6, Calculator.add("1\n2,3"));
+    }
+    @Test
+    public void testUnknownAmountOfNumbers(){
+    	assertEquals(3 + 6 + 1 + 20, Calculator.add("3, 6, 1, 20"));
+    }
 }
 
 
