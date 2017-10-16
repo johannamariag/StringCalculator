d480d849974171239b4d2d41482dbf2498ad3d3f
Johanna Maria <johannamariagislad@gmail.com>
2017-10-16 18:56:53 +0000

    refactor add, create sum method

--- src/main/java/is/ru/StringCalculator/Calculator.java
+++ src/main/java/is/ru/StringCalculator/Calculator.java
@@ -10,11 +10,7 @@ public class Calculator {
     		else{
     			if(numbers.contains(",")){
     				String N[] = numbers.split(",");
-    				int total = 0;
-    				for(String number : N){
-    					total += toInt(number);
-    				}
-    				return total;
+    				return sum(N);
     			}
     			return 1;
     		}
@@ -22,6 +18,14 @@ public class Calculator {
 
     private static int toInt(String number){
     	return Integer.parseInt(number);
+    }
+
+    private static int sum(String [] numbers){
+		int total = 0;
+    	for(String number : numbers){
+    		total += toInt(number);
+    	}
+    	return total;
     }
 
 }
